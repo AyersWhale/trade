d05e7c7ef83c5a66c1f03a0ae55ac62a03c244ea
gyx <>
2021-02-26 00:02:10 +0800

    批量merge 1

--- src/pages/demo2.js
+++ src/pages/demo2.js
@@ -8,6 +8,8 @@ export default class demo2 extends Component {
         return (
             <div>
                 this is demo2,hello 这是个初始化的create-react-app
+                2021/02/26 日更新
+                测试批量 merge  --1
             </div>
         )
     }
