0e187d7f373caf8194a5cf0c5052a20040c269fd
gyx <>
2021-06-01 10:10:48 +0800

    组件

---
+++ src/components/first.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class first extends Component {
+    render() {
+        return (
+            <div>
+                这是第一个组件
+            </div>
+        )
+    }
+}
